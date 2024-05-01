1b7d3915fa01e6404da80aeea83786425dccd4de
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-05-01 14:02:16 -0300

    feat: vector - part 1

---
+++ src/default_package/Vetor_1.java
@@ -0,0 +1,30 @@
+// Vetores - Parte 1
+// Arrays (vetores uni-dimensionais)
+
+/*
+ * numeros[indice] = elemento
+ * numero[0] = 1 -> primeiro elemento
+ * numero[1] = 3 -> segundo elemento
+ * numero[2] = 5 -> terceiro elemento
+ * numero[3] = 7 -> quarto elemento
+ * numero[4] = 9 -> quinto elemento
+ * */
+
+package default_package;
+
+public class Vetor_1 {
+	public static void main(String[] args) {
+		// Declarando um vetor
+		int vetor[];
+		
+		// Declarando e colocando tamanho no vetor
+		int vector[] = new int[5];
+		float v[] = new float[5];
+		double vt[] = new double[5];
+		String texto[] = new String[5];
+		
+		// Declarando, dimensionando e inicializando o vetor
+		String frutas[] = {"Manga", "Banana", "Maçã"};
+		int numeros[] = {1, 2, 3, 4, 5};
+	}
+}
